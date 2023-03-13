f7013eef625cf7d1a13a10add4c31028c2568fbc
Abram <58718191+4b3c@users.noreply.github.com>
2023-03-13 12:16:25 -0700

    made the rays better

--- Blurp.py
+++ Blurp.py
@@ -59,17 +59,12 @@ class blurp:
 	def draw_blurp(self, window, forward, backward, rotation):
 		self.move_with_rotation(int(forward), int(backward), rotation)
 
-		window.blit(self.sprite, self.pos)
-
-		ray_endpoint_pos = [self.speed * math.sin(math.pi * (rotation + 0.18)), \
-		self.speed * math.cos(math.pi * (rotation + 0.18))]
-		ray_endpoint_neg = [self.speed * math.sin(math.pi * (rotation - 0.18)), \
-		self.speed * math.cos(math.pi * (rotation - 0.18))]
+		ray_endpoint_pos = [[self.speed * math.sin(math.pi * (rotation + offset / 100)), self.speed * math.cos(math.pi * (rotation + offset / 100))] for offset in range(-18, 18, 3)]
 
 		x_mid = self.pos[0] + 9
 		y_mid = self.pos[1] + 13
 
-		pygame.draw.line(window, (255, 255, 255), (x_mid, y_mid), \
-			(x_mid + ray_endpoint_pos[0] * self.ray_len, y_mid + ray_endpoint_pos[1] * self.ray_len))
-		pygame.draw.line(window, (255, 255, 255), (x_mid, y_mid), \
-			(x_mid + ray_endpoint_neg[0] * self.ray_len, y_mid + ray_endpoint_neg[1] * self.ray_len))
+		for end_point in ray_endpoint_pos:
+			pygame.draw.line(window, (255, 255, 255), (x_mid, y_mid), (x_mid + end_point[0] * self.ray_len, y_mid + end_point[1] * self.ray_len))
+
+		window.blit(self.sprite, self.pos)
