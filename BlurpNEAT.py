086ee6762301a3e8bd4533d85497868529463819
Abram <58718191+4b3c@users.noreply.github.com>
2023-09-16 10:11:00 -0700

    started neat algo

---
+++ BlurpNEAT.py
@@ -0,0 +1,10 @@
+import pygame, math, random
+
+
+class Network:
+	def __init__(self, size=[6, 8, 2]):
+		self.weights = [random.random() for _ in range]
+
+class Blurp:
+	def __init__(self, pos, parents=None):
+
