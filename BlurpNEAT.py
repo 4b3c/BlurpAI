1df11515aadc02bbf1cac0ec8f9754fd36c60e57
Abram <58718191+4b3c@users.noreply.github.com>
2023-09-16 17:33:03 -0700

    finished feed forward part of NEAT

--- BlurpNEAT.py
+++ BlurpNEAT.py
@@ -1,10 +1,40 @@
-import pygame, math, random
+import pygame
+import numpy as np
 
+def sigmoid(z):
+	return 1.0/(1.0+np.exp(-z))
 
 class Network:
-	def __init__(self, size=[6, 8, 2]):
-		self.weights = [random.random() for _ in range]
+	def __init__(self, size=[6, 4, 2]):
+		self.weights = [np.random.randn(size[count], size[count + 1]) for count in range(len(size[1:]))]
+		self.values = [np.zeros(row) for row in size]
+		self.output = self.values[-1]
+
+	def feedforward(self, _input):
+		self.values[0] = _input
+		for count, weights in enumerate(self.weights):
+			new_value = np.dot(self.values[count], weights)
+			clamped_value = sigmoid(new_value)
+			self.values[count + 1] = clamped_value
+
+		self.output = self.values[-1]
+
+
+
 
 class Blurp:
 	def __init__(self, pos, parents=None):
+		pass
 
+
+
+
+
+
+bean = Network()
+# for weights in bean.weights:
+# 	print(weights)
+
+bean.feedforward([1, 1, 0, 0, 0, 0])
+
+print(bean.output)
